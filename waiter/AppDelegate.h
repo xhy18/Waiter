107fdc4997637c9cdc2addf708d6df04474a1e9c
任欣 <renxin@qinhuandeimac.local>
2019-03-18 10:42:28 +0800

    Initial Commit

---
+++ waiter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  waiter
+//
+//  Created by renxin on 2019/3/18.
+//  Copyright © 2019年 renxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ waiter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  waiter
+//
+//  Created by renxin on 2019/3/18.
+//  Copyright © 2019年 renxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
